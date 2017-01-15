fc2d3226704f5deaa40488dea3e0a2b002916189
Alex <aleqsd@gmail.com>
2017-01-15 10:24:02 +0100

    Modification de la structure pour répondre à l'énoncé : Création d'un projet de type ClassLibrary avec nos classes Création d'un projet ConsoleApplication qui utilise les classes de ClassLibrary

---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Avec un tableau
+            Console.WriteLine("Avec un tableau : ");
+            Console.WriteLine("");
+            Figure[] Figures = new Figure[2];
+            Figures[0] = new Cercle(10);
+            Figures[1] = new Rectangle(3, 4, 15, 10);
+
+
+            Figures[0].Informations();
+            Console.WriteLine("A une surface de " + String.Format("{0:0.00}", Figures[0].Surface()));
+
+            Figures[1].Informations();
+            Console.WriteLine("A une surface de " + String.Format("{0:0.00}", Figures[1].Surface()));
+            double surfaceTotale = Figures[0].Surface() + Figures[1].Surface();
+            Console.WriteLine("La surface totale de toutes les figures est : " + String.Format("{0:0.00}", surfaceTotale));
+            Console.WriteLine("");
+
+            // Avec une liste
+            Console.WriteLine("Avec une liste : ");
+            Console.WriteLine("");
+            List<Figure> Figures2 = new List<Figure>();
+            Figures2.Add(new Cercle(10));
+            Figures2.Add(new Rectangle(3, 4, 15, 10));
+
+            Figures2[0].Informations();
+            Console.WriteLine("A une surface de " + String.Format("{0:0.00}", Figures[0].Surface()));
+
+            Figures2[1].Informations();
+            Console.WriteLine("A une surface de " + String.Format("{0:0.00}", Figures[1].Surface()));
+            Console.WriteLine("La surface totale de toutes les figures est : " + String.Format("{0:0.00}", surfaceTotale));
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
